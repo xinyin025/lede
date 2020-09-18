8d74143a515884282a2710135046736c784102e2
John Xi <xinyin025@163.com>
2020-09-18 18:16:46 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,7 +10,7 @@ sed -i 's/192.168.1.1/192.168.199.10/g' package/base-files/files/bin/config_gene
 
 #拦截广告的app
 
-cd package
-mkdir openwrt-packages
-cd openwrt-packages
-git clone https://github.com/Leo-Jo/luci-app-koolproxyR.git
+#cd package
+#mkdir openwrt-packages
+#cd openwrt-packages
+#git clone https://github.com/Leo-Jo/luci-app-koolproxyR.git
